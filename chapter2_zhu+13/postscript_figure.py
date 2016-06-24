a9d9bf29d8e71bf343032e795cf1467c437b28f6
cczhu <cczhu@astro.utoronto.ca>
2016-06-24 16:58:56 -0400

    more

--- chapter2_zhu+13/postscript_figure.py
+++ chapter2_zhu+13/postscript_figure.py
@@ -59,7 +59,7 @@ def get_runaway_line(od, ax, td, od_wwk04_i=False, wwkmfc="k", color="red", wwkm
 
 def getzhu13pme_sub(files, filedir="/home/cczhu/SunnyvaleOutput/zhu16/"):
 
-	outp = {"Mtot": np.zeros(len(files)), "Mce": np.zeros(len(files)), "Mdisk": np.zeros(len(files)), 
+	outp = {"Mtot": np.zeros(len(files)), "Mce": np.zeros(len(files)), "Mdisk": np.zeros(len(files)), "MNi": np.zeros(len(files)), 
 			"Mtot_new": np.zeros(len(files)), "Txy": np.zeros([len(files),2]), "Tz": np.zeros([len(files),2]),
 			"Tc_before": np.zeros([len(files),2]), "Tc_after": np.zeros([len(files),2]),
 			"Txy_before": np.zeros([len(files),2])}
@@ -75,9 +75,13 @@ def getzhu13pme_sub(files, filedir="/home/cczhu/SunnyvaleOutput/zhu16/"):
 		outp["Tz"][i,0] = data["zTmax_new"][0]; outp["Tz"][i,1] = data["zTmax_new"][1]
 		outp["Tc_before"][i,0] = data["dens_before"][0]; outp["Tc_before"][i,1] = data["temp_before"][0]
 		outp["Tc_after"][i,0] = data["dens_after"][0]; outp["Tc_after"][i,1] = data["temp_after"][0]
-
+		outp["MNi"][i] = get_my_MNi(data)
 
 	return outp
+
+
+def get_my_MNi(data):
+		return data["M_arr"][min(np.where(data["rho_arr"] < 1e7)[0])]/1.9891e33	
 
 
 def load_ji():
@@ -116,7 +120,7 @@ files = ["pt4pt4o.p","pt4pt5o.p","pt5pt5o.p","pt4pt55o.p","pt5pt55o.p","pt55pt55
 outp = getzhu13pme_sub(files)
 outp["Mtot_new"] /= 1.9891e33
 
-for item in ["Mtot", "Mce", "Mdisk", "Mtot_new"]:
+for item in ["Mtot", "Mce", "Mdisk", "Mtot_new", "MNi"]:
 	data[item] = pd.Series(outp[item])
 data["mrho_xy"] = pd.Series(outp["Txy"][:,0])
 data["mT_xy"] = pd.Series(outp["Txy"][:,1])
@@ -136,8 +140,12 @@ labellist = ["0.4", "0.5", "0.55", "0.6", "0.65", "0.7", "0.8", "0.9", "1.0"]
 Macc = np.array([ 0.40231300000000003,  0.502649,  0.553285,  0.603529,  0.653632,  0.703745, 0.80457,  0.904897,  1.005611])
 
 p_ar = cPickle.load(open("/home/cczhu/SunnyvaleOutput/zhu16/pt625pt65AREPO.p", 'r'))
+p_ar["MNi"] = get_my_MNi(p_ar)
 ji13 = load_ji()
 schw12 = schwab_vals()
+
+
+########################## Mconv #############################
 
 width=6.; height=5.; axes=[10**5.5,10**8.5,10**7.8,10**9.75]
 
@@ -181,3 +189,96 @@ ax.set_ylabel(r"$M_\mathrm{c, pv}$ ($M_\odot$)")
 print "mean(Mtot_new - Mtot)_schw12 = {0:.3e}".format(np.mean(abs(schw12["Mtot_new"] - fitfunc(schw12["Mtot"]))))
 
 pylab.savefig("figures/c2a_mcpv.pdf", dpi=150)
+
+
+########################## Mconv #############################
+
+width=6.; height=5.; axes=[10**5.5,10**8.5,10**7.8,10**9.75]
+
+pylab.rc('font', family="serif")
+pylab.rc('font', size=14)
+fig = pylab.figure(figsize=(width,height))
+ax = fig.add_axes([0.8/width, 0.75/height, (width - 1.2)/width, (height - 1.)/height])
+
+zhu13_options = {"marker": "o", "ls": "None"}
+arepo_options = {"mfc": "b", "mec": "r", "marker": "*", "ls": "None", "mew": 2}
+schwab_options = {"mec": "m", "marker": "x", "ls": "None", "mew": 3}
+ji_options = {"mec": "c", "marker": "x", "ls": "None", "mew": 3}
+fitline_options = {"color": "k", "ls": ":", "lw": "2"}
+
+for i in range(len(Macc)):
+	args = abs(data["M2 (Msun) "] - Macc[i])/Macc[i] < 1e-6
+	ax.plot(data.loc[args, "qrho"], data.loc[args, "Mtot_new"]/data.loc[args, "Mce"], mfc=colorlist[i], **zhu13_options)
+ax.plot(np.array([data["qrho"][15]]), p_ar["Mtot_new"]/p_ar["Mce"]/1.9891e33, markersize=20, **arepo_options)
+ax.plot(np.array([data["qrho"][34]]), schw12["Mtot_new"][4]/data["Mce"][34], markersize=10, **schwab_options)
+#ax.plot(np.array([1.2]), np.array(ji13["m"][max(np.where(ji13["r"] <= 2e9)[0])]/1.9891e33), markersize=10, **ji_options)
+ax.plot(np.array([data["qrho"][9]]), np.array([1.0675/1.1]), markersize=10, **ji_options)	# From Ji, private communication
+
+line_artists = (mlines.Line2D([],[], mfc="k", ms=10, **zhu13_options),
+				mlines.Line2D([],[], ms=20, **arepo_options),
+				mlines.Line2D([],[], ms=10, **schwab_options),
+				mlines.Line2D([],[], ms=10, **ji_options))
+leg = ax.legend(line_artists, ("Zhu+13 Estimate", "Zhu+15 Arepo", "Schwab+12", "Ji+13"), loc=1, fontsize=13, numpoints = 1, markerscale=1)
+leg.draw_frame(False)
+ax.set_xlabel(r"$\rho_\mathrm{c,d}/\rho_\mathrm{c,a}$")
+ax.tick_params(axis='x', which='major', pad=7)
+ax.set_ylabel(r"$M_\mathrm{c, pv}/M_\mathrm{ce}$")
+ax.axis([0.0,1.05,0.7,1.2])
+
+pylab.savefig("figures/c2a_McMce_vs_qrho.pdf", dpi=150)
+
+########################## MNi Mtot #############################
+
+# Childress points
+childress = pd.read_table(open("/home/cczhu/GitHubTemp/PaperRunaway/Childress2015.dat", 'r'), sep=" ")
+
+# Scalzo line
+mtot_scalz, m56ni_scalz = np.array([0.8, 0.95, 1.6]),  np.array([0.35, 0.4, 0.6])
+
+# Zero temperature points
+mass_zero = cPickle.load(open("/media/DataStorage3/Runaway/outputs/massplot/cold_mass_line.p", 'r'))
+
+width=9.; height=9.; axes=[10**5.5,10**8.5,10**7.8,10**9.75]
+
+pylab.rc('font', family="serif")
+pylab.rc('font', size=18)
+fig = pylab.figure(figsize=(width,height))
+ax = fig.add_axes([1.0/width, 1.0/height, (width - 1.4)/width, (height - 1.4)/height])
+
+childress_options = {"mfc": "green", "mec": "None", "marker": "*", "ls": "None"}
+zero_options = {"color": "#b30059", "ls": ":"}
+scalz_options = {"color": "r", "ls": ":"}
+ax.plot(mass_zero["Mtot"], mass_zero["MNi"], lw=5, **zero_options)
+ax.plot(mtot_scalz, m56ni_scalz, lw=5, **scalz_options)
+ax.plot(childress["Mej"], childress["MNi"], ms=20, **childress_options)
+
+est_options = {"color": "k", "marker": 'o', "ls": "None", "markersize": 15}
+arepo_options = {"mfc": "b", "mec": "r", "marker": "*", "ls": "None", "mew": 2, "markersize": 20}
+
+for i in range(len(Macc)):
+	args = abs(data["M2 (Msun) "] - Macc[i])/Macc[i] < 1e-6
+	ax.plot(data.loc[args, "Mtot"], data.loc[args, "MNi"], mfc=colorlist[i], **zhu13_options)
+ax.plot(p_ar["Mtot"], p_ar["MNi"], **arepo_options)
+
+#ax.plot(mass_ad["Mtot"], mass_ad["MNi"], lw=5, ms=6, **ad_options)
+#ax.plot(mass_s["Mtot"], mass_s["MNi"], lw=5, ms=6, **s_options)
+
+ax.set_xlim(0.8,1.5); ax.set_ylim(0., 1.6)
+ax.set_xlabel(r"$M_\mathrm{tot}$ ($M_\odot$)", fontsize=24, labelpad=6)
+ax.set_ylabel(r"$M_\mathrm{Ni}$ ($M_\odot$)", fontsize=24, labelpad=6)
+
+line_artists = (mlines.Line2D([],[], ms=15, **childress_options),
+				mlines.Line2D([],[], lw=5, **scalz_options),
+				mlines.Line2D([],[], lw=5, **zero_options),
+				mlines.Line2D([],[], lw=5, **est_options),
+				mlines.Line2D([],[], lw=5, **arepo_options))
+leg = ax.legend(line_artists, ("Childress+ 2015", "Scalzo+ 2014", "Cold WD", "Zhu+13 Estimate", "Zhu+15 Arepo"), loc=2, fontsize=18, numpoints = 1, markerscale=1)
+leg.draw_frame(False)
+
+# Create systematic error bars
+systematic_bars = {"color": "#6699ff", "ls": "-", "lw": 5}
+ax.plot([0.925, 0.925], [0.75, 1.15], **systematic_bars)
+ax.plot([0.825, 1.025], [0.95, 0.95], **systematic_bars)
+ax.text(0.95, 1.025, "Systematic\nErrors", fontsize=18, color=systematic_bars["color"])
+
+pylab.savefig('figures/c2a_MNi.pdf', dpi = 200, edgecolor = 'w')
